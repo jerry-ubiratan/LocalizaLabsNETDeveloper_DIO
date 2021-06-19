e501790dc8abf685d99424833404fb7487cc7650
Jerry Ubiratan de Souza <jerry.ubiratan@gmail.com>
2021-06-18 23:44:44 -0300

    03-Desafios aritméticos em C#

---
+++ 03-Desafios aritméticos em C#/Program.cs
@@ -0,0 +1,327 @@
+﻿using System;
+
+namespace Aritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcao;
+            do
+            {
+                opcao = menu();
+                switch (opcao)
+                {
+                    case "1":
+                        calcMedia();
+                        break;
+                    case "2":
+                        CrescimentoPopulacional();
+                        break;
+                    case "3":
+                        Bazinga();
+                        break;
+                    case "4":
+                        TempoEvento();
+                        break;
+                    case "5":
+                        Piralandia();
+                        break;
+                };
+
+            } while (opcao != "X");
+
+
+        }
+
+        private static string menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C#");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Cálculo de Média");
+            Console.WriteLine("2 - Crescimento Populacional");
+            Console.WriteLine("3 - Bazinga!!");
+            Console.WriteLine("4 - Tempo de um Evento");
+            Console.WriteLine("5 - Comunicação em Piralândia");
+            Console.WriteLine("X - Sair do Desafio");
+            Console.WriteLine("");
+            Console.Write("Digiter sua opção=> ");
+            return Console.ReadLine().ToUpper();
+
+        }
+
+        // Calculo de Consumo médio 
+        private static void calcMedia()
+        {
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C# - Cálculo de Média ");
+            Console.WriteLine();
+
+            double a, b, mediaP;
+            try
+            {
+                Console.Write("Digite a primeira nota => ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite a segunda nota => ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                //complete com as variaveis
+                mediaP = ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5);
+
+                Console.WriteLine("MÉDIA = {0}", mediaP.ToString("0.00000"));
+                Console.WriteLine();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Parâmetros inválidos ....");
+                Console.WriteLine();
+            }
+
+            Console.Write("Digite qualquer tecla para continuar => ");
+            Console.ReadLine();
+        }
+
+        private static void CrescimentoPopulacional()
+        {
+
+            double[] arrayList = new double[4];
+            int pa, pb;
+            double cpa, cpb;
+            int anos;
+            int t = 0;
+
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C# - Crescimento Populacional ");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de testes que pretende realizar =>");
+            try
+            {
+                t = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Parâmetros > PA-População A , PB-População B, G1-Percentual de crescimento A e G2-Percentual de crescimento B ");
+                Console.WriteLine("Exemplo de entrada: PA PB G1 G2");
+                Console.WriteLine("100 150 1,0 0");
+                Console.WriteLine();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Parâmetros inválidos ....");
+            }
+
+            for (int i = 0; i < t; i++)
+            {
+                try
+                {
+                    anos = 0;
+                    Console.Write("Digite os dados => ");
+
+                    string[] valores = Console.ReadLine().Split();
+                    pa = int.Parse(valores[0]);
+                    pb = int.Parse(valores[1]);
+
+                    //declare as variaveis corretamente
+                    cpa = double.Parse(valores[2]) / 100;
+                    cpb = double.Parse(valores[3]) / 100;
+
+                    while (pa <= pb)
+                    {
+                        //complete o while
+                        pa += (int)(pa * cpa);
+                        pb += (int)(pb * cpb);
+                        anos++;
+
+                        if (anos > 100)
+                        {
+                            Console.WriteLine("Mais de 1 seculo.");
+                            break;
+                        }
+                    }
+
+                    if (anos <= 100)
+                    {
+                        Console.WriteLine($"{anos} anos.");
+                    }
+
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Parâmetros inválidos ....");
+                    Console.Write("Digite qualquer tecla para continuar => ");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+            Console.Write("Digite qualquer tecla para continuar => ");
+            Console.ReadLine();
+
+        }
+
+
+        private static void Bazinga()
+        {
+            int qtdTeste = 0;
+            string[] cond = { "tesoura papel lagarto", "papel pedra spock", "pedra lagarto tesoura", "lagarto spock papel", "spock tesoura pedra" };
+            string v1 = "", v2 = "";
+            string bazinga = "papel, pedra, tesoura, lagarto, spock";
+
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C# - Bazinga!! ");
+            Console.WriteLine();
+
+            Console.Write("Digite o número de jogadas que pretende realizar =>");
+            try
+            {
+                qtdTeste = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite as escolhas de Sheldon e de Raj");
+                Console.WriteLine($"Parâmetros possíveis ({bazinga}) ");
+                Console.WriteLine("Exemplo de entrada: lagarto pedra");
+                Console.WriteLine();
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Parâmetros inválidos ....");
+            }
+
+            for (int i = 1; i <= qtdTeste; i++) //insira a variavel correta
+            {
+                bool leftWin = false;
+                Console.Write("Digite as escolhas => ");
+                try
+                {
+                    string[] valores = Console.ReadLine().ToLower().Split();
+                    v1 = valores[0];
+                    v2 = valores[1];
+                }
+                catch (System.Exception) { }
+
+                if (!bazinga.Contains(v1) || !bazinga.Contains(v2))
+                {
+                    Console.WriteLine($"Parâmetros inválidos escolha bazinga ({bazinga})");
+                    continue;
+                }
+
+                if (v1 == v2)
+                {
+                    Console.WriteLine("Caso #{0}: De novo!", i);
+                    continue;
+                }
+
+                foreach (var c in cond)
+                {
+                    string[] cd = c.Split();
+                    int pV1 = Array.IndexOf(cd, v1);
+                    int pV2 = Array.IndexOf(cd, v2);
+                    if (pV1 == 0)
+                    {
+                        if (pV2 != -1)
+                        {
+                            leftWin = true;
+                        }
+                        break;
+                    }
+                }
+
+                if (leftWin)
+                {
+                    Console.WriteLine("Caso #{0}: Bazinga!", i);
+                }
+                else
+                {
+                    Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
+                }
+            }
+
+            Console.WriteLine("Digite qualquer tecla para continuar => ");
+            Console.ReadLine();
+        }
+
+        private static void TempoEvento()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C# - Bazinga!! ");
+            Console.WriteLine();
+
+            int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
+            int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTerminio;
+
+            try
+            {
+
+                Console.Write("Digite o dia de inicio (exemplo Dia 2) => ");
+                string[] dadosInicio = Console.ReadLine().Split();
+                diaInicio = Convert.ToInt32(dadosInicio[1]);
+
+                Console.Write("Digite a hora de inicio (exemplo HH:MM:SS ) => ");
+                string[] dadosMomentoInicio = Console.ReadLine().Split(":");
+                horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
+                minutoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[1]);
+                segundoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[2]);
+
+                Console.Write("Digite o dia que termina (exemplo Dia 5) => ");
+                string[] dadosTermino = Console.ReadLine().Split();
+                diaTermino = Convert.ToInt32(dadosTermino[1]);
+
+                Console.Write("Digite a hora que termina (exemplo HH:MM:SS ) => ");
+                string[] dadosMomentoTermino = Console.ReadLine().Split(":");
+                horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
+                minutoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[1]);
+                segundoMomentoTerminio = Convert.ToInt32(dadosMomentoTermino[2]);
+
+                DateTime datainicio = new DateTime(DateTime.Today.Year, DateTime.Today.Month, diaInicio, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio);
+                DateTime datafinal = new DateTime(DateTime.Today.Year, DateTime.Today.Month, diaTermino, horaMomentoTermino, minutoMomentoTermino, segundoMomentoTerminio);
+
+                TimeSpan dados = datafinal - datainicio;
+
+                Console.WriteLine();
+                Console.WriteLine("Duração do evento :");
+                Console.WriteLine("{0} dia(s)", dados.Days);
+                Console.WriteLine("{0} hora(s)", dados.Hours);
+                Console.WriteLine("{0} minuto(s)", dados.Minutes);
+                Console.WriteLine("{0} segundo(s)", dados.Seconds);
+                Console.WriteLine();
+
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Parâmetros inválidos ....");
+            }
+
+            Console.WriteLine("Digite qualquer tecla para continuar => ");
+            Console.ReadLine();
+
+        }
+
+        private static void Piralandia()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Desafios aritméticos em C# - Comunicação em Piralândia ");
+            Console.WriteLine();
+
+            try
+            {
+                Console.Write("Digite um número => ");
+                string n = Console.ReadLine();
+                //continue a solução
+                char[] arr = n.ToCharArray();
+                Array.Reverse(arr);
+
+                string v = new string(arr);
+                Console.WriteLine(v);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Parâmetros inválidos ....");
+            }
+
+            Console.WriteLine("Digite qualquer tecla para continuar => ");
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
---
+++ 03-Desafios aritméticos em C#/README.md
@@ -0,0 +1,90 @@
+# 03 - Desafios aritméticos em C#
+
+---
+## Desafio (Cálculo Média)
+Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+## Entrada
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+
+## Saída
+Calcule e imprima a variável MEDIA conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+---
+## Desafio (Crescimento Populacional)
+O Governador do Estado quer saber sobre as taxas de crescimento das cidades. De acordo com dados da população e a taxa de crescimento de duas cidades quaisquer (A e B), ele quer que você calcule quantos anos levará para que a cidade menor (A) cresça mais em população que a cidade maior (B).
+
+Claro que o Governador quer saber apenas para as cidades cuja taxa de crescimento da cidade A é maior do que a taxa de crescimento da cidade B, portanto, previamente já separou para você apenas os casos de teste que tem a taxa de crescimento maior para a cidade A.
+
+Desenvolva um programa que apresente o tempo, em anos, para cada caso.
+
+Porém, em alguns casos o tempo pode ser muito grande, e o Governador não se interessa em saber exatamente o tempo para estes casos, já que ele pode não estar mais eleito. Basta que você informe, nesta situação, a mensagem "Mais de 1 século.".
+
+## Entrada
+A primeira linha da entrada contém um único inteiro T, indicando o número de casos de teste (1 ≤ T ≤ 3000). Cada caso de teste contém 4 números: dois inteiros PA e PB (100 ≤ PA < 1000000, PA < PB ≤ 1000000) indicando respectivamente a população de A e B, e dois valores G1 e G2 (0.1 ≤ G1 ≤ 10.0, 0.0 ≤ G2 ≤ 10.0, G2 < G1) com um digito após o ponto decimal cada, indicando respectivamente o crescimento populacional de A e B (em percentual).
+
+Atenção: A população é sempre um valor inteiro, portanto, um crescimento de 2.5 % sobre uma população de 100 pessoas resultará em 102 pessoas, e não 102.5 pessoas, enquanto um crescimento de 2.5% sobre uma população de 1000 pessoas resultará em 1025 pessoas. Além disso, não utilize variáveis de precisão simples para as taxas de crescimento.
+
+
+## Saída
+Imprima, para cada caso de teste, quantos anos levará para que a cidade A ultrapasse a cidade B em número de habitantes. Obs.: se o tempo for mais do que 100 anos o programa deve apresentar a mensagem: Mais de 1 seculo. Neste caso, é melhor interromper o programa imediatamente após passar de 100 anos, caso contrário você poderá receber como resposta da submissão deste problema "Time Limit Exceeded".
+
+---
+## Desafio  (Bazinga!)
+Em um episódio da aclamada série The Big Ban Theor dois pernagens, Sheldon e Raj, discutem qual dos dois é o melhor: o filme Saturn 3 ou a série Deep Space 9. A sugestão de Raj para a resolução do impasse é uma disputa de Pedra-Papel-Tesoura. Contudo, Sheldon argumenta que, se as partes envolvidas se conhecem, entre 75% e 80% das disputas de Pedra-Papel-Tesoura terminam empatadas, e então sugere o Pedra-Papel-Tesoura-Lagarto-Spock.
+
+As regras do jogo proposto são:
+
+    1. a tesoura corta o papel;
+    2. o papel embrulha a pedra;
+    3. a pedra esmaga o lagarto;
+    4. o lagarto envenena Spock;
+    5. Spock destrói a tesoura;
+    6. a tesoura decapita o lagarto;
+    7. o lagarto come o papel;
+    8. o papel contesta Spock;
+    9. Spock vaporiza a pedra;
+    10. a pedra quebra a tesoura.
+
+Conhecendo os personagens, sabemos que caso Sheldon vencesse, ele gritaria a vitória com um "Bazinga!". Se Raj vencesse, Sheldon o acusaria de ter trapaceado. Agora, se desse empate, Sheldon não descansaria e insistira para jogarem de novo até que seja decidido. Sabendo dessas ações, faça um programa que imprima a provável reação de Sheldon.
+
+### Entrada 
+A entrada consiste em uma série de casos de teste. A primeira linha contém um inteiro positivo T (T ≤ 100), que representa o número de casos de teste. Cada caso de teste é representado por uma linha da entrada, contendo as escolhas de Sheldon e Raj, respectivamente, separadas por um espaço em branco. As escolhas possíveis são as personagens do jogo: pedra, papel, tesoura, lagarto e Spock.
+
+## Saída 
+Para cada caso de teste deverá ser impressa a mensagem "Caso #t: R", onde t é o número do caso de teste (cuja contagem se inicia no número um) e R é uma das três reações possíveis de Sheldon: "Bazinga!", "Raj trapaceou!", ou "De novo!".
+
+---
+## Desafio  (Tempo de um Evento!)
+Pedrinho está organizando um evento em sua Universidade. O evento deverá ser no mês de Abril, iniciando e terminando dentro do mês. O problema é que Pedrinho quer calcular o tempo que o evento vai durar, uma vez que ele sabe quando inicia e quando termina o evento.
+
+Sabendo que o evento pode durar de poucos segundos a vários dias, você deverá ajudar Pedrinho a calcular a duração deste evento.
+
+### Entrada 
+Como entrada, na primeira linha vai haver a descrição “Dia”, seguido de um espaço e o dia do mês no qual o evento vai começar. Na linha seguinte, será informado o momento no qual o evento vai iniciar, no formato hh : mm : ss. Na terceira e quarta linha de entrada haverá outra informação no mesmo formato das duas primeiras linhas, indicando o término do evento.
+
+## Saída 
+Na saída, deve ser apresentada a duração do evento, no seguinte formato:
+
+W dia(s)
+X hora(s)
+Y minuto(s)
+Z segundo(s)
+
+Obs: Considere que o evento do caso de teste para o problema tem duração mínima de 1 minuto.
+
+---
+## Desafio  (Comunicação em Piralândia)
+Os cientistas brasileiros descobriram um exoplaneta a 1 bilhão de anos luz da terra. Ele foi carinhosamente batizado de Piralândia, uma homenagem a cidade de Piracicaba, onde moram os cientistas. Após algum tempo observando esse planeta, os cientistas puderam ver que nele haviam habitantes e, além disso, esses habitantes estavam tentando fazer contato com os cientistas através de uma comunicação numérica. 
+
+Só que a numeração que encontraram estava invertida e como muitas delas foram descobertas, os cientistas chamaram você para conseguir automatizar esse processo. Logo, dado um número grande, sua tarefa é imprimir esse número invertido.
+
+### Entrada 
+O arquivo contém apenas uma linha de teste que é o número encontrado (0 < n < 9999999999).
+
+Obs.: Perceba que o número lido é muito alto para armazenar em uma variável do tipo int, logo você irá precisar utilizar o tipo long, que para a leitura e impressão em C, você deve utilizar o %llu.
+
+## Saída 
+mprimir o número lido invertido. Não esqueça de imprimir a quebra de linha (\n) no final, caso contrário você receberá (Presentation Error).
+
+--- 
